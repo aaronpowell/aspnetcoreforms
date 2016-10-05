f661b59cbf1a6ff33960cfa88c6a28d9bdbc6d9c
Aaron Powell <me@aaron-powell.com>
2016-10-05 11:35:56 +1100

    so much winning

---
+++ src/AspNetCoreForms/ParseException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspNetCoreForms
+{
+    public class ParseException : Exception
+    {
+        public ParseException()
+            : base ("Parser Error: The Runat attribute must have the value Server.")
+        {
+        }
+    }
+}
---
+++ src/AspNetCoreForms/RunAtTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCoreForms
+{
+    [HtmlTargetElement(Attributes="runat")]
+    public class RunAtTagHelper : TagHelper
+    {
+        public string RunAt { get; set; }
+
+        public override void Init(TagHelperContext context)
+        {
+            if (string.Compare(RunAt, "server", System.StringComparison.OrdinalIgnoreCase) != 0)
+                throw new ParseException();
+
+            base.Init(context);
+        }
+    }
+}
